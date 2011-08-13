808d3db10356eece6065230be55cc43b6e173df2
Danny Su <dannysu@live.com>
2011-08-13 00:16:25 -0400

    Add code to prevent user interaction on lists while refreshing data

---
+++ MangaStream/Converters/InverseBooleanConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Data;
+using System.Globalization;
+
+namespace MangaStream
+{
+    public class InverseBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            bool result = System.Convert.ToBoolean(value, CultureInfo.InvariantCulture);
+
+            return !result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
